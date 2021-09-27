7ab7fa9bb9b8de8096456e27ee1902727cd9d5f9
RogueNPC <jordanbtansay@gmail.com>
2021-09-26 22:41:31 -0700

    [ADD] added username feature

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var incr = 1;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  // when a user enters, emits connection msg
   socket.broadcast.emit('enter', 'a new user has connected.');
+
+  // grants users guest name as default
+  socket.emit('name', "guest"+incr);
+  incr++;
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 
+  // when a user leaves, emits disconnection msg
   socket.on('disconnect', () => {
     socket.broadcast.emit('leave', 'a user has disconnected.');
+    incr--;
   });
 });
 
